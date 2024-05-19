5c4f106aeb01e83b242e2e5fdb3fba3171ecd47b
Perveev Evgen <perveev.evgen@gmail.com>
2024-05-19 21:34:08 +0300

    add 2FA helpers

---
+++ data/two-factor-token.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+
+const getTwoFactorTokenByToken = async (token: string) => {
+  try {
+    const twoFactorToken = await db.twoFactorToken.findUnique({
+      where: { token },
+    });
+    return twoFactorToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+const getTwoFactorTokenByEmail = async (email: string) => {
+  try {
+    const twoFactorToken = await db.twoFactorToken.findFirst({
+      where: { email },
+    });
+    return twoFactorToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+export { getTwoFactorTokenByEmail, getTwoFactorTokenByToken };
