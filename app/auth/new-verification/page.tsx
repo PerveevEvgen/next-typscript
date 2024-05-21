6238554d55c32000ee8d286cd4cef4dbd6736289
Perveev Evgen <perveev.evgen@gmail.com>
2024-05-21 09:06:56 +0300

    create error and verification pages

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,7 @@
+import { NewVerifocationForm } from "@/components/auth/new-verification-form";
+
+const NewVerificationPage = () => {
+  return <NewVerifocationForm />;
+};
+
+export default NewVerificationPage;
